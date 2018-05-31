480a8385598d231fe9978d76ff53005c274ea7ba
Dewald928 <26013835@student.g.nwu.ac.za>
2018-05-31 02:47:06 +0200

    die ding?

--- app/routes.js
+++ app/routes.js
@@ -17,29 +17,23 @@ module.exports = function(app, passport) {
     	let query = connection.query(sql, (err, results) => {
         if(err) throw err;
 		// console.log(results);
+			res.render('courses.ejs', {
 				course : results //get courses for each user
 			});
     	});
 	});
 	
+	//adds course to user
 	app.get('/courses/add/:id', function(req, res){
 		// console.log(req.user.id);
 		let sql = 'INSERT INTO enrollment (STUDENT_ID, COURSE_ID) VALUES (?,?)';
 		let studentID = req.user.id;
 		let courseid = parseInt(req.params.id);
+		//*check if couse already added
     	let query = connection.query(sql,[studentID, courseid], (err, results) => {
         if(err) throw err;
 		// console.log(results);
         res.redirect('/profile');
-    	});
-	});
-	
-	app.get('/availableCourses', function(req, res){
-		let sql = 'SELECT * FROM courses';
-    	let query = connection.query(sql, (err, results) => {
-        if(err) throw err;
-		console.log(results);
-		res.json(results); //get courses for each user
     	});
 	});
 	
@@ -61,8 +55,7 @@ module.exports = function(app, passport) {
 		let courseDesc = req.body.courseDescription;
     	let query = connection.query(sql,[courseName, courseDesc], (err, results) => {
         if(err) throw err;
-		console.log(results);
-		res.json(results); //get courses for each user
+        res.redirect('/profile');
     	});
 	});
 
@@ -153,7 +146,6 @@ module.exports = function(app, passport) {
 			if(isEmpty(mycourses)) {
 				mycourses = "";
 			} 
-			console.log(mycourses);
 			res.render('profile.ejs', {
 				user : req.user, // get the user out of session and pass to template
 				course : mycourses //get courses for each user
--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // config/database.js
 module.exports = {
     'connection': {
-        // 'host': '196.252.167.121',
-        // 'user': 'dewald',
-        'host' : 'localhost',
-        'user': 'root',
+        'host': '196.252.167.121',
+        'user': 'dewald',
+        // 'host' : 'localhost',
+        // 'user': 'root',
         'password': ''
     },
 	'database': 'db_practical',
